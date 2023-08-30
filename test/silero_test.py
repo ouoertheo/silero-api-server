9457f50189297300603db8b6fcc3885f54268cf1
ouoertheo <jamin@tomeofjamin.net>
2023-08-29 19:17:57 -0500

    ignore models, tests

--- test/silero_test.py
+++ test/silero_test.py
@@ -1,9 +1,15 @@
 import unittest
 from silero_api_server.tts import SileroTtsService
-from silero_api_server.server import SAMPLE_PATH
 
 class SileroApiServerTest(unittest.TestCase):
-    def test_get_languages(self):
-        svc = SileroTtsService(SAMPLE_PATH)
-        lang = svc.get_languages()
+    def test_list_languages(self):
+        svc = SileroTtsService('samples')
+        lang = svc.list_languages()
         self.assertIn('https://models.silero.ai/models/tts/es/v3_es.pt',lang.values())
+
+    def test_init_model(self):
+        svc = SileroTtsService('samples')
+        lang = svc.list_languages()
+        svc.load_model(list(lang.keys())[0])
+        model_file = svc.model_file
+        self.assertTrue(model_file.exists())
